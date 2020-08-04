4a996f8bb5407380ae881c468c75fa36a719b7df
AlexanderCarlston <alekscarlston@gmail.com>
2020-08-04 15:48:58 -0600

    Switching to diff repo

---
+++ App Users/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Users.Models
+{
+    public class Class
+    {
+        public int Identifier { get; set; }
+        public string UserName { get; set; }
+    }
+}
